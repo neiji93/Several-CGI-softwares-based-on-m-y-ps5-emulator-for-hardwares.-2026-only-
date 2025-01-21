2439d048b35c14cb9b3511c0ceac3ab423c7c27c
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-21 08:15:26 +0100

    Update TDBRDFEmulator.hlsl

--- TDBRDFEmulator.hlsl
+++ TDBRDFEmulator.hlsl
@@ -1,1 +1,6 @@
 
+//we have to prevent using 2024-2025stuffs.
+//a way to do that is use xompiter vision.
+//but here we have to emulate canceling albedo of svbrdf.
+//another thing to do will be transform.them as post processing.
+//....
